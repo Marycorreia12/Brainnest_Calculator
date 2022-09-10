16f3c9036ec3fc6c6c112f0d2cb099bdf587c489
Maria Santos <correia_nety@hotmail.com>
2022-09-10 12:38:50 +0100

    Calculator working!

--- js/script.js
+++ js/script.js
@@ -1,60 +1,167 @@
-function add(num1, num2){
-    return num1 + num2;
+/* Add two numbers */
+function add(firstNumber, secondNumber){
+    return firstNumber + secondNumber;
 }
-
-let sum = add(8, 9);
-console.log(sum);
-
-function subtract(num1, num2){
-    return num1 - num2;
+/* subtract two numbers */
+function subtract(firstNumber, secondNumber){
+    return firstNumber - secondNumber;
 }
-
-let sub = subtract(8, 6);
-console.log(sub);
-
-
-function multiply(num1, num2){
-    return num1 * num2;
+/* multiply two numbers */
+function multiply(firstNumber, secondNumber){
+    return firstNumber * secondNumber;
 }
-
-let mult = multiply(6, 6);
-console.log(mult);
-
-
-function divide(num1, num2){
-    return num1 / num2;
+/* divide two numbers */
+function divide(firstNumber, secondNumber){
+    return firstNumber / secondNumber;
 }
-
-let divid = divide(8, 6);
-console.log(divid);
-
-function operate(operator, num1, num2){
-    if(operator == '+'){
-        return add(num1, num2);
+/* Make the operations */
+function operate(operator, firstNumber, secondNumber){
+    if(operator === '+'){
+        return add(firstNumber, secondNumber);
     }
-    else if(operator == '-'){
-        return subtract(num1, num2);
+    else if(operator === '-'){
+        return subtract(firstNumber, secondNumber);
     }
-    else if(operator == '*'){
-        return multiply(num1, num2);
+    else if(operator === '*'){
+        return multiply(firstNumber, secondNumber);
     }
-    else if(operator == '/'){
-        return divide(num1, num2);
+    else if(operator === '/'){
+        return divide(firstNumber, secondNumber);
     }
-    else 
-        return operator;
+    return secondNumber;
 }
 
-let testingOperator = operate('*', 25, 5);
-console.log(testingOperator);
+const display = document.querySelector('.input');
 
-function display(){
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach((button) => {
-        button.addEventListener('click', () => {
-            console.log(button.value);
-        });
-    }) ;
+const calculator = {
+    displayValue: '0',
+    firstNumber: null,
+    operator: null,
+    secondNumber: false,
+};
+
+function clear() {
+    calculator.displayValue = '0';
+    calculator.firstNumber = null;
+    calculator.operator = null;
+    calculator.secondNumber = false;
+}       
+
+function updateDisplay() {
+    display.value = calculator.displayValue;
 }
 
-let resultDisplay = display();
+const buttons = document.querySelector('.buttons');
+buttons.addEventListener('click', (e) => {
+const target = e.target;
+
+    if (!target.matches('button')) {
+        return;
+    }
+
+    if (target.classList.contains('clear')) {
+        clear();
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains('delete')) {
+        undo();
+        return;
+    }
+
+    if (target.classList.contains('plusMinus')) {
+        plusMinus();
+        return;
+    }
+
+    if (target.classList.contains('percent')) {
+        percentage();
+        return;
+    }
+
+    if (target.classList.contains('decimal')) {
+        decimal(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains('operator')) {
+        calculate(target.value);
+        updateDisplay();
+        return;
+    }
+
+displayNumber(target.value);
+updateDisplay();
+});
+
+function undo(){
+    display.value = display.value.slice(0, -1);
+    calculator.displayValue = display.value;
+}
+
+function plusMinus(){
+    let displayPlusMinus = calculator.displayValue;
+    if(displayPlusMinus >= 0){
+        display.value = '-' + displayPlusMinus;    
+    }
+    calculator.displayValue = display.value;
+}
+
+function percentage(){
+    let displayPercent = calculator.displayValue;
+    if(displayPercent != 0){
+        display.value = displayPercent / 100;
+    }
+   calculator.displayValue = display.value;
+}
+
+function decimal(dot) {
+    if (calculator.secondNumber === true) {
+        calculator.displayValue = '0.'
+        calculator.secondNumber = false;
+        return
+    }
+    if (!calculator.displayValue.includes(dot)) {
+        calculator.displayValue += dot;
+    }
+}
+
+function displayNumber(number) {
+    let displayValue = calculator.displayValue;
+    const secondNumber = calculator.secondNumber;
+    
+    if (secondNumber === true) {
+        calculator.displayValue = number;
+        calculator.secondNumber = false;
+    }
+    else if(displayValue === '0'){
+        calculator.displayValue = number;
+    }
+    else{
+        calculator.displayValue = displayValue + number;
+    }
+}
+
+function calculate(inputOperator) {
+    const firstNumber = calculator.firstNumber;
+    const displayValue = calculator.displayValue;
+    const operator  = calculator.operator;
+   
+    const inputValue = parseFloat(displayValue);
+
+    if (firstNumber == null) {
+        calculator.firstNumber = inputValue;
+
+    } else if (operator) {
+        const result = operate(operator, firstNumber, inputValue);
+        calculator.displayValue = `${parseFloat(result.toFixed(6))}`;
+        calculator.firstNumber = result;
+    }
+    calculator.secondNumber = true;
+    calculator.operator = inputOperator;
+    console.log(calculator);
+}
+
+    
